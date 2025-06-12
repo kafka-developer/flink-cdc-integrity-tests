c266dd4e8bf0008c36ec07ebbe75da4dc9e68a7f
kafka-developer <79891381+kafka-developer@users.noreply.github.com>
2025-06-12 10:53:11 -0400

    Create Sink.sql

---
+++ Sink.sql
@@ -0,0 +1,20 @@
+CREATE TABLE SINK_ORPHAN_CI_ADR (
+  CI_ID STRING,
+  CI_A1_1 STRING,
+  ERROR_REASON STRING,
+  PRIMARY KEY (CI_ID) NOT ENFORCED
+) WITH (
+  'connector' = 'confluent',
+  'kafka.topic' = 'orphan_ci_adr',
+  'format' = 'avro-registry'
+);
+
+INSERT INTO SINK_ORPHAN_CI_ADR
+SELECT
+  adr.CI_ID,
+  adr.CI_A1_1,
+  'NO MATCH FOUND' AS ERROR_REASON
+FROM SOURCE_CBA_CI_ADR adr
+LEFT JOIN SOURCE_CBA_CI ci
+  ON adr.CI_ID = ci.CI_ID
+WHERE ci.CI_ID IS NULL;
