c8ccda29db0822662621993bffb298cae97bac19
kafka-developer <79891381+kafka-developer@users.noreply.github.com>
2025-06-12 10:51:50 -0400

    Create Source.sql

---
+++ Source.sql
@@ -0,0 +1,19 @@
+CREATE TABLE SOURCE_CBA_CI (
+  CI_ID STRING,
+  CI_STATE_C STRING,
+  PRIMARY KEY (CI_ID) NOT ENFORCED
+) WITH (
+  'connector' = 'confluent',
+  'kafka.topic' = 'cba_ci',
+  'format' = 'avro-registry'
+);
+
+CREATE TABLE SOURCE_CBA_CI_ADR (
+  CI_ID STRING,
+  CI_A1_1 STRING,
+  PRIMARY KEY (CI_ID) NOT ENFORCED
+) WITH (
+  'connector' = 'confluent',
+  'kafka.topic' = 'cba_ci_adr',
+  'format' = 'avro-registry'
+);
